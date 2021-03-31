c57a042cf4715866ab657b5f46788e3b15f0887e
Giancarlo Anemone <ganemone@uber.com>
2021-03-31 16:38:52 -0400

    Increase default max old space size for yarn commands (#33)
    * Increase default max old space size for yarn commands
    
    * Update commands/yarn.js
    
    Co-authored-by: Chris Deacy <deacy@uber.com>
    
    * fix flow
    
    Co-authored-by: Chris Deacy <deacy@uber.com>

--- commands/yarn.js
+++ commands/yarn.js
@@ -15,6 +15,12 @@ type Yarn = (YarnArgs) => Promise<void>
 */
 const runYarn /*: Yarn */ = async ({cwd, args = [], stdio = 'inherit'}) => {
   const params = [yarn, ...getPassThroughArgs(args)];
+  if (typeof process.env.NODE_OPTIONS !== 'string') {
+    process.env.NODE_OPTIONS = '--max_old_space_size=8192';
+  } else if (!process.env.NODE_OPTIONS.includes('--max_old_space_size')) {
+    // $FlowFixMe
+    process.env.NODE_OPTIONS += ' --max_old_space_size=8192';
+  }
   await spawn(node, params, {env: process.env, cwd, stdio});
 };
 
