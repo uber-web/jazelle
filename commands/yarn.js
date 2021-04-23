068ff4c1fbcf8b60f5eb0c01bf58b172fa079cc0
Giancarlo Anemone <ganemone@uber.com>
2021-04-23 13:22:33 -0400

    Always set NODE_OPTIONS when spawning procs (#37)

--- commands/yarn.js
+++ commands/yarn.js
@@ -15,13 +15,7 @@ type Yarn = (YarnArgs) => Promise<void>
 */
 const runYarn /*: Yarn */ = async ({cwd, args = [], stdio = 'inherit'}) => {
   const params = [yarn, ...getPassThroughArgs(args)];
-  if (typeof process.env.NODE_OPTIONS !== 'string') {
-    process.env.NODE_OPTIONS = '--max_old_space_size=8192';
-  } else if (!process.env.NODE_OPTIONS.includes('--max_old_space_size')) {
-    // $FlowFixMe
-    process.env.NODE_OPTIONS += ' --max_old_space_size=8192';
-  }
-  await spawn(node, params, {env: process.env, cwd, stdio});
+  await spawn(node, params, {cwd, stdio});
 };
 
 module.exports = {yarn: runYarn};
