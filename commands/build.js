56b9e4efd67492adf13b6eb2e673541be364fea6
Leo Horie <leohorie@hotmail.com>
2021-05-06 09:56:38 -0700

    add support for hasSandboxIO config property to manifest.json, which makes `jz build` not error if the underlying command causes IO writes in build sandbox (#40)

--- commands/build.js
+++ commands/build.js
@@ -1,5 +1,6 @@
 // @flow
 const {assertProjectDir} = require('../utils/assert-project-dir.js');
+const {getManifest} = require('../utils/get-manifest.js');
 const {executeProjectCommand} = require('../utils/execute-project-command.js');
 
 /*::
@@ -14,7 +15,20 @@ export type Build = (BuildArgs) => Promise<void>
 const build /*: Build */ = async ({root, cwd, stdio = 'inherit'}) => {
   await assertProjectDir({dir: cwd});
 
-  await executeProjectCommand({root, cwd, command: 'build', stdio});
+  const {hasSandboxIO} = await getManifest({root});
+
+  if (hasSandboxIO) {
+    // don't throw error on jz build if build configuration is known to write to sandbox
+    await executeProjectCommand({
+      root,
+      cwd,
+      command: 'script',
+      args: ['build'],
+      stdio,
+    });
+  } else {
+    await executeProjectCommand({root, cwd, command: 'build', stdio});
+  }
 };
 
 module.exports = {build};
