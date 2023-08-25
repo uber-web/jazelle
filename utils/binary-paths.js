d9ce0d0d7542f38b515f925a8487ddcdc5c0c5d1
Leo Horie <lhorie@uber.com>
2023-08-25 10:27:31 -0700

    fix test

--- utils/binary-paths.js
+++ utils/binary-paths.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const {getRootDir} = require('../utils/get-root-dir');
 
 const paths = {
-  bazel:
-    String(process.env.BAZELISK_PATH) ||
-    String(process.env.BAZEL) ||
-    `${__dirname}/../bin/bazelisk`,
+  bazel: String(
+    process.env.BAZELISK_PATH ||
+      process.env.BAZEL ||
+      `${__dirname}/../bin/bazelisk`
+  ),
   node: String(process.argv[0]),
   // TODO: Fix this now with yarn v2
-  yarn: String(process.env.YARN) || `${__dirname}/../bin/yarn.js`, // this env var is created by rules/jazelle.bzl, the yarn binary is put there by preinstall hook
+  yarn: String(process.env.YARN || `${__dirname}/../bin/yarn.js`), // this env var is created by rules/jazelle.bzl, the yarn binary is put there by preinstall hook
 };
 
 try {
